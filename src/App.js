6e83103df17267a648a8829d27bf2a23c98709de
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-15 17:58:23 -0300

    iniciando o formulario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Banner from './componentes/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario';
 
 function App() {
   //Banner é um component q foi importado
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Formulario />
     </div>
   );
 }
