5f066df0f832770bc0385f3e54f61e7f054d89bc
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-15 21:38:05 -0300

    criando a ListaSuspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,17 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa';
 import "./Formulario.css";
 
 const Formulario = () => {
+const itens =[
+  "Teste1",
+  "Teste2",
+  "Teste2",
+  "Teste3",
+  "Teste4",
+  "Teste5"
+]
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +19,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={itens} />
       </form>
     </section>
   );
