9db3cec4a4df4f9ee976f7fb4c44153e4445673f
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-03-23 16:11:07 -0300

    Iniciando a parte de colaboradores

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,15 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (<div className='colaborador'>
+        <div className='cabecalho'>
+            <img src='https://github.com/kaueslo.png' alt=' Kaue Sloboda'/>
+        </div>
+        <div className='rodape'>
+            <h4>Naruto Uzumaki</h4>
+            <h5>Folha</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
--- src/componentes/Item/index.js
+++ src/componentes/Item/index.js
@@ -1,9 +1,11 @@
+import Colaborador from '../Colaborador'
 import './Item.css'
 
 const Item = (props) => {
     return (
         <section className='item' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+            <Colaborador />
         </section>
     )
 }
