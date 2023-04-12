06953326542087a568e63ac8746af763ee722ff9
Kaue Sloboda <kaue.sloboda@hotmail.com>
2023-04-11 22:00:11 -0300

    iniciando o aoDeletar

--- src/componentes/Item/index.js
+++ src/componentes/Item/index.js
@@ -1,12 +1,14 @@
 import Colaborador from '../Colaborador'
 import './Item.css'
 
-const Item = (props) => {
+const Item = (props, aoDeletar) => {
     return (
        (props.usuarios.length > 0) ? <section className='item' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
-                {props.usuarios.map(usuario => <Colaborador corDeFundo={props.corPrimaria} key={usuario.nome} nome={usuario.nome} item={usuario.item} imagem={usuario.imagem}/>)}
+                {props.usuarios.map(usuario => {
+                    return <Colaborador corDeFundo={props.corPrimaria} key={usuario.nome} nome={usuario.nome} item={usuario.item} imagem={usuario.imagem} aoDeletar={aoDeletar} />
+                })}
             </div>
         </section>
         :  ''
